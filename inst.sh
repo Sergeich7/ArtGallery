90c367938bf39793d824a0193091b0ff107a5467
Vitaly Belashov <p3@yandex.ru>
2022-12-03 13:31:49 +0600

    Production without celery

--- inst.sh
+++ inst.sh
@@ -5,11 +5,11 @@ source .env  > /dev/null 2>&1
 docker-compose down
 docker-compose up --build --no-start
 
-docker-compose start mysql web nginx certbot
-
 if [[ $STATE == *"DEV"* ]]; then
     # запускаем асинхронность только для разработки
-    # на production - слабый сервер
-    # можно будет быстро поодключить при необходимости
-    docker-compose start rabbit redis celery flower phpmyadmin
+    docker-compose up
+else
+    # на production слабый сервер. можно будет быстро
+    # поодключить асинхронность при необходимости
+    docker-compose start mysql web nginx certbot
 fi
